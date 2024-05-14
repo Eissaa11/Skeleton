0f9829b34fa968b81f1daac6a6d81c582e2e5992
Md Efaz <P2783025@my365.dmu.ac.uk>
2024-05-14 11:35:44 +0100

    Added testing for Reservation class

---
+++ Testing5/tstReservation.cs
@@ -0,0 +1,17 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing5
+{
+    [TestClass]
+    public class tstReservation
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsReservation Reservation = new clsReservation();
+            Assert.IsNotNull(Reservation);
+        }
+    }
+}
