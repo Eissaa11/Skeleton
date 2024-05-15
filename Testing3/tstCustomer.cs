e58333d0a1960cd7122377080d072db6c87a9275
Ahmed  Bahar EISSA <168041507+Eissaa11@users.noreply.github.com>
2024-05-15 21:05:16 +0100

    Hi guys i made my first test

---
+++ Testing3/tstCustomer.cs
@@ -0,0 +1,18 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing3
+{
+    [TestClass]
+    public class tstCustomer
+        
+    {
+        [TestMethod]
+        public void InstanceOKc()
+        {
+            ClsCustomer AnCustomer = new ClsCustomer();
+            Assert.IsNotNull(AnCustomer);
+        }
+    }
+}
