1a1239b9cfdbf6b2ae20983a1440b8cc56ef558b
Md Efaz <P2783025@my365.dmu.ac.uk>
2024-05-16 14:21:50 +0100

    Added testing for all classes of mine

--- ClassLibrary/clsReservation.cs
+++ ClassLibrary/clsReservation.cs
@@ -1,6 +1,16 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsReservation
     {
+        public bool Active { get; set; }
+        public bool Age { get; set; }
+        public DateTime DateAndTime { get; set; }
+        public int ReservationId { get; set; }
+        public int CustomerId { get; set; }
+        public string Name { get; set; }
+        public object Phone { get; set; }
+        public int TableNumber { get; set; }
     }
 }
--- Testing5/tstReservation.cs
+++ Testing5/tstReservation.cs
@@ -13,5 +13,61 @@ namespace Testing5
             clsReservation Reservation = new clsReservation();
             Assert.IsNotNull(Reservation);
         }
+        [TestMethod]
+        public void AgePropertyOK()
+        {
+            clsReservation Reservation = new clsReservation();
+            Boolean TestData = true;
+            Reservation.Age = TestData;
+            Assert.AreEqual(Reservation.Age, TestData);
+        }
+        [TestMethod]
+        public void DateAndTimePropertyOK()
+        {
+            clsReservation Reservation = new clsReservation();
+            DateTime TestData = DateTime.Now.Date;
+            Reservation.DateAndTime = TestData;
+            Assert.AreEqual(Reservation.DateAndTime, TestData);
+        }
+        [TestMethod]
+        public void ReservationIdPropertyOK()
+        {
+            clsReservation Reservation = new clsReservation();
+            Int32 TestData = 1;
+            Reservation.ReservationId = TestData;
+            Assert.AreEqual(Reservation.ReservationId, TestData);
+        }
+        [TestMethod]
+        public void CustomerIdPropertyOK()
+        {
+            clsReservation Reservation = new clsReservation();
+            Int32 TestData = 1;    
+            Reservation.CustomerId = TestData;
+            Assert.AreEqual(Reservation.CustomerId, TestData);
+        }
+        [TestMethod]
+        public void NamePropertyOK()
+        {
+            clsReservation Reservation = new clsReservation();
+            string TestData = "Md Jobaer Hasan Efaz";
+            Reservation.Name = TestData;
+            Assert.AreEqual(Reservation.Name, TestData);
+        }
+        [TestMethod]
+        public void PhonePropertyOK()
+        {
+            clsReservation Reservation = new clsReservation();
+            string TestData = "07737279924";
+            Reservation.Phone = TestData;
+            Assert.AreEqual(Reservation.Phone, TestData);
+        }
+        [TestMethod]
+        public void TableNumberPropertyOK()
+        {
+            clsReservation Reservation = new clsReservation();
+            Int32 TestData = 1;
+            Reservation.TableNumber = TestData;
+            Assert.AreEqual(Reservation.TableNumber, TestData);
+        }
     }
 }
