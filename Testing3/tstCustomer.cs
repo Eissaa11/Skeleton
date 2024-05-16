05cec63a6b4024c3de9fa1b2a45cc4af52ffae78
P2735061 <P2735061@CLPX0027WIN072W>
2024-05-16 15:51:53 +0100

    All test done and all it pass

--- Testing3/tstCustomer.cs
+++ Testing3/tstCustomer.cs
@@ -6,7 +6,7 @@ namespace Testing3
 {
     [TestClass]
     public class tstCustomer
-        
+
     {
         [TestMethod]
         public void InstanceOKc()
@@ -14,5 +14,71 @@ namespace Testing3
             ClsCustomer AnCustomer = new ClsCustomer();
             Assert.IsNotNull(AnCustomer);
         }
+        [TestMethod]
+        public void GenderPropertryOK()
+        {
+            ClsCustomer AnCustomer = new ClsCustomer();
+            Boolean TestData = true;
+            AnCustomer.Gender = TestData;
+            Assert.AreEqual(AnCustomer.Gender, TestData);
+        }
+        [TestMethod]
+        public void CustomerIdPropertryOK()
+        {
+            ClsCustomer AnCustomer = new ClsCustomer();
+            Int32 TestData = 1;
+            AnCustomer.CustomerId = TestData;
+            Assert.AreEqual(AnCustomer.CustomerId, TestData);
+        }
+        [TestMethod]
+        public void FirstNamePropertryOK()
+        {
+            ClsCustomer AnCustomer = new ClsCustomer();
+            String TestData = "Ahmed";
+            AnCustomer.FirstName = TestData;
+            Assert.AreEqual(AnCustomer.FirstName, TestData);
+        }
+        [TestMethod]
+        public void LastNamePropertryOK()
+        {
+            ClsCustomer AnCustomer = new ClsCustomer();
+            String TestData = "Ahmed";
+            AnCustomer.LastName = TestData;
+            Assert.AreEqual(AnCustomer.LastName, TestData);
+        }
+        [TestMethod]
+        public void EmailPropertryOK()
+        {
+            ClsCustomer AnCustomer = new ClsCustomer();
+            String TestData = "Ahmedbahar911@gmail.com";
+            AnCustomer.Email = TestData;
+            Assert.AreEqual(AnCustomer.Email, TestData);
+
+        }
+        [TestMethod]
+        public void PhonePropertryOK()
+        {
+            ClsCustomer AnCustomer = new ClsCustomer();
+            Int32 TestData = 1;
+            AnCustomer.phoneno = TestData;
+            Assert.AreEqual(AnCustomer.phoneno, TestData);
+        }
+        [TestMethod]
+        public void AddressPropertryOK()
+        {
+            ClsCustomer AnCustomer = new ClsCustomer();
+            String TestData = "61 canonsligh road";
+            AnCustomer.Address = TestData;
+            Assert.AreEqual(AnCustomer.Address, TestData);
+        }
+        [TestMethod]
+        public void OrderdetaIdPropertryOK()
+        {
+            ClsCustomer AnCustomer = new ClsCustomer();
+            DateTime TestData = DateTime.Now.Date;
+            AnCustomer.Orderdate = TestData;
+            Assert.AreEqual(AnCustomer.Orderdate, TestData);
+        }
     }
+
 }
