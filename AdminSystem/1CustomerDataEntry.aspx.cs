0fdf79ef27328d89e8a5912639f9a2c509ebf207
P2735061 <P2735061@CLPX0027WIN072W>
2024-05-21 12:32:42 +0100

    hi

--- AdminSystem/1CustomerDataEntry.aspx.cs
+++ AdminSystem/1CustomerDataEntry.aspx.cs
@@ -16,12 +16,13 @@ public partial class _1_DataEntry : System.Web.UI.Page
 
     protected void btnOK_Click(object sender, EventArgs e)
     {
+
         //crate a new instance of cLsCustomer
         ClsCustomer AnCustomer = new ClsCustomer();
-        AnCustomer.LastName =TextBox3.Text;
-        AnCustomer.Email =txtemail.Text;
-        AnCustomer.phone = txtphoneno.Text;
-        AnCustomer.Address = txtaddress.Text;
+        AnCustomer.LastName = textlname.Text;
+        AnCustomer.Email = txtemail.Text;
+        AnCustomer.phone = txtphone.Text;
+        AnCustomer.Address = textaddress.Text;
         AnCustomer.orderdate = Convert.ToDateTime(DateTime.Now);
         AnCustomer.Gender = chkfemale.Checked;
         AnCustomer.Gender = chkmale.Checked;
@@ -30,23 +31,14 @@ public partial class _1_DataEntry : System.Web.UI.Page
         //navigate to the view page
         Response.Redirect("1CustomerViewer.aspx");
 
+    }
+    protected void btnacncel_Click(object sender, EventArgs e)
+    {
 
     }
 
-    protected void Button1_Click(object sender, EventArgs e)
+    protected void TextBox8_TextChanged(object sender, EventArgs e)
     {
-        //crate a new instance of cLsCustomer
-        ClsCustomer AnCustomer = new ClsCustomer();
-        AnCustomer.LastName = TextBox3.Text;
-        AnCustomer.Email = txtemail.Text;
-        AnCustomer.phone = txtphoneno.Text;
-        AnCustomer.Address = txtaddress.Text;
-        AnCustomer.orderdate = Convert.ToDateTime(DateTime.Now);
-        AnCustomer.Gender = chkfemale.Checked;
-        AnCustomer.Gender = chkmale.Checked;
-        //store the address in the session object
-        Session["AnCustomer"] = AnCustomer;
-        //navigate to the view page
-        Response.Redirect("1CustomerViewer.aspx");
+
     }
 }
