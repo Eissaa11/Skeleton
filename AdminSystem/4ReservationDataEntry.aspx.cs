ab3750ee6694032b80937d8a57ceadd24e2473b0
Md Efaz <P2783025@my365.dmu.ac.uk>
2024-05-21 12:41:02 +0100

    done presentation layer for reseravtion

--- AdminSystem/4ReservationDataEntry.aspx.cs
+++ AdminSystem/4ReservationDataEntry.aspx.cs
@@ -1,13 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel.Configuration;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_DataEntry : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void bttnOk_Click(object sender, EventArgs e)
+    {
+        //create a new instance of clsReservation
+        clsReservation Reservation = new clsReservation();
+        //capture the name
+        Reservation.Name = txtName.Text;
+        Reservation.Phone = txtPhone.Text;
+        Reservation.ReservationId = Convert.ToInt32(txtReservationId.Text);
+        Reservation.CustomerId = Convert.ToInt32(txtCustomerId.Text);
+        Reservation.TableNumber = Convert.ToInt32(txtTableNumber.Text);
+        Reservation.DateAndTime = Convert.ToDateTime(DateTime.Now);
+        Reservation.Age = chkAge.Checked;
+        //store the name in the session object
+        Session["Reservation"] = Reservation;
+        //navigate to the view page//
+        Response.Redirect("4ReservationViewer.aspx");
+    }
+
+    protected void txtReservationId_TextChanged(object sender, EventArgs e)
     {
 
     }
