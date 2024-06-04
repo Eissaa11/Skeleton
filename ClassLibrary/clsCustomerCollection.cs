7af9ec7b592fe0b1f67add16e697becb632628f4
P2735061 <P2735061@CLPX0027WIN072W>
2024-06-04 01:50:03 +0100

    Creating the Collection Class

---
+++ ClassLibrary/clsCustomerCollection.cs
@@ -0,0 +1,11 @@
+﻿namespace ClassLibrary
+{
+    public class clsCustomerCollection
+    {
+        public clsCustomerCollection()
+        {
+        }
+
+        public System.Collections.Generic.List<clsCustomer> CustomerList { get; set; }
+    }
+}
