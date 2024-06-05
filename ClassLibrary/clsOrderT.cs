a94a179166fbfa649f498c0501e7d6715d08f21e
p2786882 <p2786882@my365.dmu.ac.uk>
2024-06-06 00:29:00 +0100

    dhs

--- ClassLibrary/clsOrderT.cs
+++ ClassLibrary/clsOrderT.cs
@@ -4,16 +4,141 @@ namespace ClassLibrary
 {
     public class clsOrderT
     {
-        public clsOrderT()
+
+        //private data member for the order id property
+        private Int32 mOrder_Id;
+        //Order_Id public property
+        public int Order_Id
         {
+            get
+            {
+                //this line of the code send the data out of the property
+                return mOrder_Id;
+            }
+            set
+            {
+                //this line of code alloiws data into the property
+                mOrder_Id = value;
+            }
+        }
+        //private data member for the CustomerId property
+        private Int32 mCustomerId;
+        //CustomerId public property
+        public int CustomerId
+        {
+            get
+            {
+                //this line of the code send the data out of the property
+                return mCustomerId;
+            }
+            set
+            {
+
+                //this line of code alloiws data into the property
+                mCustomerId = value;
+            }
+        }
+        //private data member for the Paid property
+        private bool mPaid;
+
+        //Paid public property
+        public bool Paid
+        {
+            get
+            { //this line of the code send the data out of the property
+                return mPaid;
+            }
+            set
+            {//this line of code allows data into the property
+                mPaid = value;
+            }
         }
 
-        public bool Paid { get; set; }
-        public int Order_Id { get; set; }
-        public int CustomerId { get; set; }
-        public DateTime Placed_at { get; set; }
-        public string Firstname { get; set; }
-        public int Total_amount { get; set; }
-        public int phoneno { get; set; }
+          
+
+       
+        //private data member for the Placed_at property
+        private DateTime mPlaced_at;
+        //Placed_at public property
+        public DateTime Placed_at
+        {
+            get
+            {
+                //this line of code sends data out of property
+                return mPlaced_at;
+            }
+
+            set
+            {
+                //this line of code alloiws data into the property
+                mPlaced_at = value;
+            }
+        }
+        //private data member for the Firstname property   
+        private string mFirstname;
+        //Firstname public property
+        public string Firstname
+        {
+            get
+            {
+                //this line of the code send the data out of the property; set; }
+                return mFirstname;
+            }
+            set
+            {
+                //this line of code alloiws data into the property
+                mFirstname = value;
+            }
+        }
+
+        //private data member for the Total_amount property
+        private Int32 mTotal_amount;
+
+        //Total_amount public property  
+        public int Total_amount
+        {
+            get
+            {
+                //this line of the code send the data out of the property; set; }
+                return mTotal_amount;
+            }
+            set
+            {
+                //this line of code alloiws data into the property
+                mTotal_amount = value;
+            }
+        }
+
+
+        //private data member for the Phoneno property    
+        private Int32 mPhoneno;
+        //Phoneno  public property
+        public int Phoneno
+        {
+            get
+            {
+                //this line of the code send the data out of the property
+                return mPhoneno; 
+            }
+            set
+            {
+                //this line of code alloiws data into the property
+                mPhoneno = value;
+            }
+
+        }
+                public bool Find(int orderId)
+        {
+            //set the private data members to the test data value
+            mOrder_Id = 17;
+            mPlaced_at = Convert.ToDateTime("23/12/2022");
+            mFirstname = "Alex";
+            mTotal_amount = 10;
+            mCustomerId = 11;
+            mPaid = true;
+            mPhoneno = 0123456789;
+            //always true
+            return true;
+        }
     }
 }
--- Testing4/tstOrderT.cs
+++ Testing4/tstOrderT.cs
@@ -1,6 +1,7 @@
 ﻿using ClassLibrary;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Net;
 
 namespace Testing4
 {
@@ -91,19 +92,175 @@ namespace Testing4
             Assert.AreEqual(AnOrderT.Total_amount, TestData);
         }
         [TestMethod]
-        public void phonenoPropertyOK()
+        public void PhonenoPropertyOK()
         {
             //create an instance of the class we want to create
             clsOrderT AnOrderT = new clsOrderT();
             //create some test data to assign to the property
             Int32 TestData = 1;
             //assign the data to the property
-            AnOrderT.phoneno = TestData;
+            AnOrderT.Phoneno = TestData;
             //test to see that the two values are the same
-            Assert.AreEqual(AnOrderT.phoneno, TestData);
+            Assert.AreEqual(AnOrderT.Phoneno, TestData);
         }
+        [TestMethod]
+        public void FindMethodOK()
+        {
+            //Create an  instance of the class we want to create
+            clsOrderT AnOrderT = new clsOrderT();
+            //create a boolean variable to store the result of the validation
+            Boolean Found = false;
+            //Create some test data to use with the method
+            Int32 Order_Id = 17;
+            //invoke the method
+            Found = AnOrderT.Find(Order_Id);
+            //test to see if the result is true
+            Assert.IsTrue(Found);
+        }
+        [TestMethod]
+        public void TestOrderIdFound()
+        {
+            //Create an instance of the class we want to create
+            clsOrderT AnOrderT = new clsOrderT();
+            //create a boolean variable to store the result of the search
+            Boolean Found = false;
+            //create a boolean variable to record if the data ia OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 Order_Id = 17;
+            //invoke the method
+            Found = AnOrderT.Find(Order_Id);
+            // check the OrderID
+            if (AnOrderT.Order_Id != 17)
+            {
+                OK = false;
+            }
+            //test to see the result is correct
+            Assert.IsTrue(OK);
 
-        
-        
+
+
+        }
+        [TestMethod]
+        public void TestPlaced_atFound()
+        {
+            //Create an instance of the class we want to create
+            clsOrderT AnOrderT = new clsOrderT();
+            //create a boolean variable to store the result of the search
+            Boolean Found = false;
+            //create a boolean variable to record if the data ia OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 Order_Id = 17;
+            //invoke the method
+            Found = AnOrderT.Find(Order_Id);
+            // check the date of order placed which is Placd_at
+
+            if (AnOrderT.Placed_at != Convert.ToDateTime("23/12/2022"))
+            {
+                OK = false;
+            }
+            //test to see the result is correct
+            Assert.IsTrue(OK);
+        }
+        [TestMethod]
+        public void TestPaidFound()
+        {
+            //Create an instance of the class we want to create
+            clsOrderT AnOrderT = new clsOrderT();
+            //create a boolean variable to store the result of the search
+            Boolean Found = false;
+            //create a boolean variable to record if the data ia OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 Order_Id = 17;
+            //invoke the method
+            Found = AnOrderT.Find(Order_Id);
+            // check that order is paid or not  
+
+            if (AnOrderT.Paid != true)
+            {
+                OK = false;
+            }
+            //test to see the result is correct
+            Assert.IsTrue(OK);
+        }
+        [TestMethod]
+        public void TestCustomerIdFound()
+        {
+            //Create an instance of the class we want to create
+            clsOrderT AnOrderT = new clsOrderT();
+            //create a boolean variable to store the result of the search
+            Boolean Found = false;
+            //create a boolean variable to record if the data ia OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 CustomerId = 11;
+            //invoke the method
+            Found = AnOrderT.Find(CustomerId);
+            // check the CustmerID
+            if (AnOrderT.CustomerId != 11)
+            {
+                OK = false;
+            }
+            //test to see the result is correct
+            Assert.IsTrue(OK);
+        }
+        [TestMethod]
+        public void TestPhonenoFound()
+        {
+            //Create an instance of the class we want to create
+            clsOrderT AnOrderT = new clsOrderT();
+            //create a boolean variable to store the result of the search
+            Boolean Found = false;
+            //create a boolean variable to record if the data ia OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 Phoneno = 0123456789;
+            //invoke the method
+            Found = AnOrderT.Find(Phoneno);
+            // check the OrderID
+            if (AnOrderT.Phoneno != 0123456789)
+            {
+                OK = false;
+            }
+            //test to see the result is correct
+            Assert.IsTrue(OK);
+        }
+       
+        [TestMethod]
+        public void TestFirstnameFound()
+        {
+            //Create an instance of the class we want to create
+            clsOrderT AnOrderT = new clsOrderT();
+            //create some test data to assign to the property
+            string TestData = "Alex";
+            //assign the data to the property
+            AnOrderT.Firstname= TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnOrderT.Firstname, TestData);
+
+        }
+        [TestMethod]
+        public void TestTotal_amountFound()
+        {
+            //Create an instance of the class we want to create
+            clsOrderT AnOrderT = new clsOrderT();
+            //create a boolean variable to store the result of the search
+            Boolean Found = false;
+            //create a boolean variable to record if the data ia OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 Total_amount = 10;
+            //invoke the method
+            Found = AnOrderT.Find(Total_amount);
+            // check the OrderID
+            if (AnOrderT.Total_amount != 10)
+            {
+                OK = false;
+            }
+            //test to see the result is correct
+            Assert.IsTrue(OK);
+        }
+        }
     }
-}
