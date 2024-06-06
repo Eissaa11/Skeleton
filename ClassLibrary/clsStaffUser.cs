d96073122a992d6ebed695fd83a875eac540327a
Aravind Amgoth <P2781951@my365.dmu.ac.uk>
2024-06-06 14:16:21 +0100

    Login page done

---
+++ ClassLibrary/clsStaffUser.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+
+namespace ClassLibrary
+{
+    public class clsStaffUser
+    {
+        private Int32 mUserId;
+        public int UserId
+        {
+            get
+            {
+                return mUserId;
+            }
+            set
+            {
+                mUserId = value;
+            }
+        }
+        private string mUserName;
+        public string UserName
+        {
+            get
+            {
+                return mUserName;
+            }
+            set
+            {
+                mUserName = value;
+            }
+        }
+        private string mPassword;
+        public string Password
+        {
+            get
+            {
+                return mPassword;
+            }
+            set
+            {
+                mPassword = value;
+            }
+        }
+        private string mDepartment;
+        public string Department
+        {
+            get
+            {
+                return mDepartment;
+            }
+            set
+            {
+                mDepartment = value;
+            }
+        }
+        public bool FindUser(string userName, string UserPassword)
+        {
+            clsDataConnection DB = new clsDataConnection();
+            DB.AddParameter("@Username", userName);
+            DB.AddParameter("@Password", UserPassword);
+            DB.Execute("tblUser_FindUsernamePW");
+            if(DB.Count == 1)
+            {
+                mUserId = Convert.ToInt32(DB.DataTable.Rows[0]["UserId"]);
+                mUserName = Convert.ToString(DB.DataTable.Rows[0]["UserName"]);
+                mPassword = Convert.ToString(DB.DataTable.Rows[0]["Password"]);
+                mDepartment = Convert.ToString(DB.DataTable.Rows[0]["Department"]);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Testing2/tstStaffUser.cs
@@ -0,0 +1,59 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing2
+{
+    [TestClass]
+    public class tstStaffUser
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsStaffUser User = new clsStaffUser();
+            Assert.IsNotNull(User);
+        }
+        [TestMethod]
+        public void UserIDPropertyOK()
+        {
+            clsStaffUser User = new clsStaffUser();
+            Int32 TestData = 1;
+            User.UserId = TestData;
+            Assert.AreEqual(User.UserId, TestData);
+        }
+        [TestMethod]
+        public void UserNamePropertyOK()
+        {
+            clsStaffUser User = new clsStaffUser();
+            string TestData = "Aravind";
+            User.UserName = TestData;
+            Assert.AreEqual(User.UserName, TestData);
+        }
+        [TestMethod]
+        public void PasswordPropertyOK()
+        {
+            clsStaffUser User = new clsStaffUser();
+            string TestData = "Aravind@123";
+            User.Password = TestData;
+            Assert.AreEqual(User.Password, TestData);
+        }
+        [TestMethod]
+        public void DepartmentPropertyOK()
+        {
+            clsStaffUser User = new clsStaffUser();
+            string TestData = "Staff Manager";
+            User.Department = TestData;
+            Assert.AreEqual(User.Department, TestData);
+        }
+        [TestMethod]
+        public void FindUserMethodOK()
+        {
+            clsStaffUser User = new clsStaffUser();
+            Boolean Found = false;
+            string UserName = "Aravind";
+            string Password = "Aravind@123";
+            Found = User.FindUser(UserName, Password);
+            Assert.IsTrue(Found);
+        }
+    }
+}
